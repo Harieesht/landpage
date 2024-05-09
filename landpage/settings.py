02eb4239150561938c5ee56eea05fed7c97e3153
Harieesh Rakhavandaran N A <110871194+Harieesht@users.noreply.github.com>
2024-05-09 18:09:39 +0530

    Update settings.py

--- landpage/settings.py
+++ landpage/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from django.core.management.utils import get_randon_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
